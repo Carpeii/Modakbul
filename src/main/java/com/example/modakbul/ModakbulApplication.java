93ef02b693aa9769d935d20cd6daefbf7119a44f
Carpeii <rlawldnd1206@gmail.com>
2026-08-26 01:43:43 +0900

    [init] 프로젝트 생성

---
+++ src/main/java/com/example/modakbul/ModakbulApplication.java
@@ -0,0 +1,13 @@
+package com.example.modakbul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModakbulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModakbulApplication.class, args);
+    }
+
+}
